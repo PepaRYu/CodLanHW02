dc3aaee57cbad696825b10d4059c836a3652d53a
DrPRun <sheighd@gmail.com>
2022-07-14 23:01:11 +0300

    Zadacha 13 done

---
+++ Zadacha010/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+string number = Console.ReadLine();
+
+
+try
+{   
+    Console.WriteLine(number[2]);
+}
+catch (System.Exception)
+{
+    Console.WriteLine("Третьей цифры нет");
+    throw;
+}
