2b453fc29c1c4948b5a74414c0671eb51beb7040
DrPRun <sheighd@gmail.com>
2022-07-14 23:57:40 +0300

    zadacha 15

---
+++ Zadacha015/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 6 || number == 7){
+    Console.WriteLine("да");
+}
+else{
+    Console.WriteLine("нет");
+}
